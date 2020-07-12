b8921c13655e7396a90ce09a0a5f19db0b83521b
biz-santo <47196565+biz-santo@users.noreply.github.com>
2020-07-13 00:52:53 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-        return "Hello, This is Santosh - EE!"
+        return "Hello EE, This is Santosh!"
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0')
